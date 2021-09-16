15c38ae2fad4906235beb90932e17e6a5e807234
mayankt18 <mt.20u10405@btech.nitdgp.ac.in>
2021-09-16 19:11:00 +0530

    changes in models

--- tracker/models.py
+++ tracker/models.py
@@ -33,7 +33,6 @@ class HealthScore(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     age = models.IntegerField(default=0)
     gender = models.CharField(choices=GENDER_CHOICES, max_length=7)
-<<<<<<< HEAD
     height = models.DecimalField(max_digits=10, decimal_places=2)
     weight = models.DecimalField(max_digits=10, decimal_places=2)
     bmi = models.DecimalField(
@@ -46,23 +45,6 @@ class HealthScore(models.Model):
     total = models.DecimalField(
         max_digits=10, decimal_places=2, blank=True, null=True)
     created = models.DateTimeField(auto_now_add=True, null=True)
-=======
-    height = models.DecimalField(max_digits=10,decimal_places=2)
-    weight = models.DecimalField(max_digits=10,decimal_places=2)
-    bmi = models.DecimalField(max_digits=10,decimal_places=2,blank=True,null=True)
-    bmitext = models.CharField(max_length=50, blank=True,null=True)
-    creatinine = models.DecimalField(max_digits=10,decimal_places=2)
-    creatininetext = models.CharField(max_length=50, blank=True,null=True)
-    bloodsugar = models.DecimalField(max_digits=10,decimal_places=2)
-    bloodsugartext = models.CharField(max_length=50, blank=True,null=True)
-    cholesterol = models.DecimalField(max_digits=10,decimal_places=2)
-    cholesteroltext = models.CharField(max_length=50, blank=True,null=True)
-    sysbp = models.DecimalField(max_digits=10,decimal_places=2)
-    diabp = models.DecimalField(max_digits=10,decimal_places=2)
-    bptext = models.CharField(max_length=50, blank=True,null=True)
-    created = models.DateTimeField(auto_now_add=True,null=True)
-    expired = models.DateTimeField(auto_now_add=False,null=True)
->>>>>>> 5e0c43c59389168dbdffb61918ade84e8609884c
 
     def __str__(self):
         return self.user.username
