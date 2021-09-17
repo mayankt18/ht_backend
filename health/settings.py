2c2c9d4fb09ba76552c30521e78238c9f225a1bb
mayankt18 <mt.20u10405@btech.nitdgp.ac.in>
2021-09-17 20:43:07 +0530

    cors error

--- health/settings.py
+++ health/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = ['pres-cribe.herokuapp.com']
 
 
 CORS_ORIGIN_ALLOW_ALL = True
-CORS_ALLOW_CREDENTIALS = False
+CORS_ALLOW_CREDENTIALS = True
 # CORS_ORIGIN_WHITELIST = (
 #     'http://localhost:3000/',
 # )
